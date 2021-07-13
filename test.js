68fcc7abced168f763ddba86bb2eaa5cd2affaa6
Joao Machado <machado@codacy.com>
2021-07-13 19:21:54 +0100

    A PR that makes the file change

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+console.log('Hello World');
+
+console.log('Hello Universe');
+
+console.log('Hello other stuff');
+
+if(foo === "bar") {
+  console.log('stuff')
+}
+
+console.log('will it trigger the others?');
