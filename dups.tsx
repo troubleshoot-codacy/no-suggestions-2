ddf539f50b7be328a80146c85d71e3d62fcb0ff7
Joana Teodoro <joana.teodoro@codacy.com>
2024-01-30 11:01:17 +0000

    Create dups.tsx

---
+++ dups.tsx
@@ -0,0 +1,27 @@
+function getTime(): number {
+  return new Date().getTime();
+}
+
+function getTime(): number {
+  return new Date().getTime();
+}
+
+function getTime(): number {
+  return new Date().getTime();
+}
+
+function printHello(): void {
+  console.log('Hello!');
+}
+
+function printHello(): void {
+  console.log('Hello!');
+}
+
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+function multiply(a: number, b: number) {
+  return a * b;
+}
