9422a9135ed33410dda2561f86b610930d705c64
Joana Teodoro <joana.teodoro@codacy.com>
2024-01-30 11:21:21 +0000

    multiplying multiply, stonks

--- dups.tsx
+++ dups.tsx
@@ -31,6 +31,14 @@ function multiply(a: number, b: number) {
   return a * b;
 }
 
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
 printHello()
 const a = multiply(2,3)
 const b = 1 + a
